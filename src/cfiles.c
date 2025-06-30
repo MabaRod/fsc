5f0af8fc6d949f961661fb1945584ac4f642dcdd
maba <mabarod25@gmail.com>
2025-06-29 22:43:25 -0700

    1.0.1

--- src/cfiles.c
+++ src/cfiles.c
@@ -12,13 +12,14 @@ void cfiles (const char *proyectName){
     chdir(proyectName);
     mkdir("src", 0777);
     mkdir("include", 0777);
-     FILE *fp = fopen("Makefile", "w");
+  FILE *fp = fopen("Makefile", "w");
    if (fp == NULL){
     perror("Error creating file");
     printf("error creating file\n");
       return;
   }
-     FILE *fp1 = fopen("Main.c", "w");
+  chdir("src");
+     FILE *fp1 = fopen("main.c", "w");
    if (fp1 == NULL){
     perror("Error creating file");
     printf("error creating file\n");
@@ -32,7 +33,6 @@ void cppfiles (const char *proyectName){
     mkdir(proyectName,0777);
     chdir(proyectName);
     mkdir("src", 0777);
-  
     mkdir("include", 0777);
      FILE *fp = fopen("Makefile", "w");
    if (fp == NULL){
@@ -40,6 +40,7 @@ void cppfiles (const char *proyectName){
     printf("error creating file\n");
       return;
   }
+    chdir("src");
      FILE *fp1 = fopen("main.cpp", "w");
    if (fp1 == NULL){
     perror("Error creating file");
